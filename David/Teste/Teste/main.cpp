abb0173164d50c56303fd5ac05ca298719ea0650
David Rodrigues <davidrod.contato@gmail.com>
2019-08-15 11:53:32 -0300

    Projeto inicial

---
+++ David/Teste/Teste/main.cpp
@@ -0,0 +1,22 @@
+  //  main.cpp
+//  Teste
+//
+//  Created by David on 15/08/2019.
+//  Copyright © 2019 David. All rights reserved.
+//
+
+#define GL_SILENCE_DEPRECATION
+
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
